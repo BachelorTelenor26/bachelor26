a7765013458a7ca8b89ea3aebe0d153befba274c
sabrina <sabrina130102@gmail.com>
2026-05-15 00:06:31 +0200

    Added sources for images

--- app/components/shared/Footer.tsx
+++ app/components/shared/Footer.tsx
@@ -1,10 +1,41 @@
 export default function Footer() {
   return (
     <footer className="border-t border-gray-100 mt-16 text-amber-50 bg-[#045a90]">
-      <div className="max-w-5xl mx-auto px-4 py-10 flex items-center justify-center">
+      <div className="max-w-5xl mx-auto px-4 py-10 flex items-center flex-col justify-center">
         <p className="text-xs text-black-500">
           © 2026 Telenor · Kunnskapsbase
         </p>
+        <div className="mt-2 text-xs text-white/70">
+          <p>Kilder:{" "}
+            <a 
+              href="https://www.magnific.com/free-photo/happy-customer-service-representative-using-computer-having-video-call-with-clients-while-working-call-center_26345279.htm#fromView=search&page=1&position=2&uuid=4a1d85fd-a0b6-4be0-9def-c2996cfda5b1&query=customer+service" 
+              title="modem icons"
+              target="_blank"
+              rel="noopener noreferrer"
+              >
+                Magnific
+            </a> {" - "}
+
+             <a 
+              href="https://www.magnific.com/free-photo/young-professionals-brainstorming-coffee-stylish-workspace_395593153.htm#fromView=search&page=2&position=47&uuid=58b4be35-2576-40a0-87f9-32991ecdbc5c&query=woman+laughing+around+coffee+table" 
+              title="modem icons"
+              target="_blank"
+              rel="noopener noreferrer"
+              >
+                Magnific
+            </a> {" - "}
+        
+          <a 
+            href="https://www.flaticon.com/free-icons/modem"
+            title="modem icons"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+              Modem icons created by everyday icon - Flaticon
+          </a>  
+          </p>
+        </div>
+        
       </div>
     </footer>
   )
